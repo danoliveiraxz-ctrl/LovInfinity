7ad098a41a26dc01461e79e6ba39e7621513a225
danoliveiraxz-ctrl <danoliveiraxz@gmail.com>
2026-09-18 12:15:11 -0300

    Use LovInfinity logo as extension icon

--- background.js
+++ background.js
@@ -1,7 +1,24 @@
+async function setLovInfinityIcon(){
+  try{
+    const res=await fetch(chrome.runtime.getURL("icon.svg"));
+    const blob=await res.blob();
+    const bitmap=await createImageBitmap(blob);
+    const imageData={};
+    for(const size of [16,32,48]){
+      const canvas=new OffscreenCanvas(size,size);
+      const ctx=canvas.getContext("2d");
+      ctx.drawImage(bitmap,0,0,size,size);
+      imageData[size]=ctx.getImageData(0,0,size,size);
+    }
+    await chrome.action.setIcon({imageData});
+    bitmap.close();
+  }catch(e){console.warn("LovInfinity icon error",e);}
+}
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.local.get(["license"], (data) => {
-    if (!data.license) {
-      chrome.storage.local.set({ license: null });
-    }
+    if (!data.license) chrome.storage.local.set({ license: null });
   });
+  setLovInfinityIcon();
 });
+chrome.runtime.onStartup.addListener(setLovInfinityIcon);
+setLovInfinityIcon();
